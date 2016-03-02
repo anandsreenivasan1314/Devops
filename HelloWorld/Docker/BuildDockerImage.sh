9cef1ac361e8c284ec78c1def37efeb7150e1f87
anandsreenivasan1314 <nithi4anand@gmail.com>
2016-03-02 16:31:22 +0530

    Build fix

--- HelloWorld/Docker/BuildDockerImage.sh
+++ HelloWorld/Docker/BuildDockerImage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #cd /root/Desktop/Docker
-cd /var/lib/jenkins/jobs/Git_Java_Project/workspace/HelloWorld/Docker
-docker build -t anandconsol/tomcat-7.0:Jenkin .
+#cd /var/lib/jenkins/jobs/Git_Java_Project/workspace/HelloWorld/Docker
+#docker build -t anandconsol/tomcat-7.0:Jenkin .
 docker run -it -p 8084:8080 consol/tomcat-7.0:Jenkin /bin/bash
